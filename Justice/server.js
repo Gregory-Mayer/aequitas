46c4f0ea140f2fd787a119e899fb71cb529c480e
Nick Sorauf <nicksorauf@gmail.com>
2018-04-18 12:29:49 -0400

    still doesn't work

--- Justice/server.js
+++ Justice/server.js
@@ -1,8 +1,11 @@
-//'use strict';
-//var http = require('http');
-//var port = process.env.PORT || 1337;
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 1337;
 
-//http.createServer(function (req, res) {
-//    res.writeHead(200, { 'Content-Type': 'text/plain' });
-//    res.end('Hello World\n');
-//}).listen(port);
+http.createServer(function (req, res) {
+	var mongoose = require('mongoose');
+	mongoose.connect('mongodb://ec2-34-230-59-155.compute-1.amazonaws.com:27017/CrimeData');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
+
--- Justice/src/App.js
+++ Justice/src/App.js
@@ -2,20 +2,63 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class TitleBar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: 'blah'
+        }
+
+        this.updateName = this.updateName.bind(this)
+    }
+    updateName(e) {
+        this.setState({
+            name: e.target.value
+        })
+    }
+    render() {
+        return (
+            <div>
+                <div id="titleBar">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="#">JUSTICE</a>
+                        </div>
+                        <ul class="nav navbar-nav">
+                            <li><a href="#">Map VIEW</a></li>
+                            <li><a href="#">Graph View</a></li>
+                            <li><a href="#">Table View</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            /*<div>
+                <h1>Your name is {this.state.name}</h1>
+                Change name:
+                    <input
+                    type="text"
+                    value={this.state.name}
+                    onChange={this.updateName}
+                />
+            </div>*/
+        )
+    }
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <TitleBar />
+        );
+    }
 }
 
 export default App;
