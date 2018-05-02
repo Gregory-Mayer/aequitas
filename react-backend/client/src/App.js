c09d797fe8df51d80ec9cdf4e972433ec72d1474
gregory-mayer <gmayer1@umbc.edu>
2018-05-02 16:15:32 -0400

    express-backend connected
    added the backend to the project now passes a crime object list to app.js off of /users

---
+++ react-backend/client/src/App.js
@@ -0,0 +1,46 @@
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// class App extends Component {
+  // render() {
+    // return (
+      // <div className="App">
+        // <header className="App-header">
+          // <img src={logo} className="App-logo" alt="logo" />
+          // <h1 className="App-title">Welcome to React</h1>
+        // </header>
+        // <p className="App-intro">
+          // To get started, edit <code>src/App.js</code> and save to reload.
+        // </p>
+      // </div>
+    // );
+  // }
+// }
+
+// export default App;
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {users: []}
+
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Crimes</h1>
+		 {this.state.users.map(user =>
+          <div key={user._id}>{user.crimedate}</div>
+        )}
+	  </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-backend/routes/CrimeModel.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+mongoose.set('debug', true)
+
+
+var CrimeSchema = new mongoose.Schema(
+	{
+	crimedate: {type: Date, required: true},
+	crimetime: {type: Date, required: true},
+	crimecode: {type: String, required: true, max: 10},
+	location: {type: String, required: true, max: 50},
+	description: {type: String, required: true, max: 50},
+	inside_outside: {type: String, enum: ['I', 'O', 'Inside', 'Outside',]},
+	weapon: {type: String, max: 25},
+	post: {type: Number, max: 5},
+	district: {type: String, required: true, max: 20},
+	neighborhood: {type: String, required: true, max: 50},
+	longitude: {type: Number, required: true, max: 20},
+	latitude: {type: Number, required: true, max: 20},
+	location_1: {type: String, max: 50},
+	premise: {type: String, max: 15},
+	total_incidents: {type: Number, required: true, max: 5}
+	}//, { collection: 'Crime' }
+);
+
+
+//Export model
+ var CrimeModel = mongoose.model('Crime', CrimeSchema);
+ module.exports = CrimeModel;
