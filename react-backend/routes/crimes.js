8689a9d0f0ade3ed58228629ebc158c543eb5bd1
gregory-mayer <gmayer1@umbc.edu>
2018-05-08 17:20:01 -0400

    cleaned up the code
    removed commented out code

--- react-backend/routes/crimes.js
+++ react-backend/routes/crimes.js
@@ -17,11 +17,6 @@ router.get('/', function(req, res, next) {
 });
 ///* post crimes listing. */
 router.post('/', function(req, res, next) {
-	//console.log("HERE");
-	//console.log(req.body);
-	// var gt = '2018-01-01';
-	// var lt = '2018-05-04';
-	//	console.log(req.body);
 	var query = {};
 	var maxDate;
 	var minDate
@@ -37,8 +32,6 @@ router.post('/', function(req, res, next) {
 	else{
 		minDate = req.body.date.start;
 	}
-	console.log(maxDate);
-	console.log(minDate);
 	var maxTime;
 	var minTime;
 	if(req.body.time.end == null || !(req.body.time.end)){
@@ -46,7 +39,7 @@ router.post('/', function(req, res, next) {
 	}
 	else{
 		maxTime = req.body.time.end + ':00:00';
-		if(maxTime.length < 8){
+		if(maxTime.length < 8){ //formats the data
 			maxTime = '0' + maxTime
 		}
 		
@@ -56,13 +49,11 @@ router.post('/', function(req, res, next) {
 	}
 	else{
 		minTime = req.body.time.start + ':00:00';
-		if(minTime.length < 8){
+		if(minTime.length < 8){//formats the data
 			minTime = '0' + minTime
 		}
 	}
 	query.crimetime = { '$lt' : maxTime, '$gt' : minTime, };
-	console.log(maxTime);
-	console.log(minTime);
 	if( req.body.description && req.body.description != '' && req.body.description != []){
 		query.description =  req.body.description;
 	}
@@ -78,43 +69,32 @@ router.post('/', function(req, res, next) {
 	if( req.body.threatlevel && req.body.threatlevel != '' && req.body.threatlevel != []){
 		query.threatlevel = { '$in' : req.body.threatlevel};
 	}
-	console.log(maxDate);
-	console.log(minDate);
 	if(query && minDate != '' && maxDate != ''){
 		Crime.find(query, function(err, crimes) {  
 			if (err){
 				res.send(err);
-			//	console.log(err);
+				console.log(err);
 			}
-			//console.log(">>>>>>>>>>>>>>>>>");
-			//var crimeList = [crimes];
-			//console.log('Query' + crimes);
 			res.json(crimes);
-			}).maxTime(10000).where('crimedate').gt(minDate).lt(maxDate);///*.where('crimetime').gt(minTime).lt(maxTime)*/.limit(5000);
+			}).maxTime(10000).where('crimedate').gt(minDate).lt(maxDate);
 	}
 	else if(query){
 		Crime.find(query, function(err, crimes) {  
 			if (err){
 				res.send(err);
-			//	console.log(err);
+				console.log(err);
 			}
-			//console.log(">>>>>>>>>>>>>>>>>");
-			//var crimeList = [crimes];
-			//console.log('Query' + crimes);
 			res.json(crimes);
-			}).maxTime(10000);//.where('crimetime').gt(minTime).lt(maxTime).limit(5000);
+			}).maxTime(10000);
 	}
 	else{
 		Crime.find({}, function(err, crimes) {  
 		if (err){
 			res.send(err);
-		//	console.log(err);
+			console.log(err);
 		}
-		//console.log(">>>>>>>>>>>>>>>>>");
-		//var crimeList = [crimes];
-		//console.log('Query' + crimes);
 		res.json(crimes);
-		}).maxTime(10000);//.limit(8000);
+		}).maxTime(10000);
 	}
 });
 
